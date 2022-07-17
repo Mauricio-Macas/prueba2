c8c81a877b0cde868c73f328bb678c3ddb990d53
Mauricio-MC1 <maomacasj@hotmail.com>
2022-07-17 13:24:13 -0500

    Mauricio-Macas

---
+++ coreDietas/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for coreDietas project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coreDietas.settings')
+
+application = get_asgi_application()
